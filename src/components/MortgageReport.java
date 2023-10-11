45f92a324c3fec43a49d7e14c1ee45e3e2dbb42a
NekoJar <mzharfanhakim7@gmail.com>
2023-10-11 23:35:16 +0700

    finishing the app

--- src/components/MortgageReport.java
+++ src/components/MortgageReport.java
@@ -4,16 +4,19 @@ import java.text.NumberFormat;
 import java.util.Locale;
 
 public class MortgageReport {
+    private final NumberFormat currency;
     private MortgageCalculator mortgageCalculator;
+
 
     public MortgageReport(MortgageCalculator mortgageCalculator) {
         this.mortgageCalculator = mortgageCalculator;
+        currency = NumberFormat.getCurrencyInstance(new Locale.Builder().setLanguage("en").setRegion("US").build());
     }
 
     public void printMortgage() {
         double mortgage = mortgageCalculator.calculateMortgage();
         
-        String mortgageFormatted = NumberFormat.getCurrencyInstance(new Locale.Builder().setLanguage("en").setRegion("US").build()).format(mortgage);
+        String mortgageFormatted = currency.format(mortgage);
         System.out.println("\nMORTGAGE");
         System.out.println("--------");
         System.out.println("Monthly Payments: " + mortgageFormatted);
@@ -23,7 +26,7 @@ public class MortgageReport {
         System.out.println("\nPAYMENT SCHEDULE");
         System.out.println("--------");
         for(double balance : mortgageCalculator.getRemainingBalance()){
-            String balanceFormatted = NumberFormat.getCurrencyInstance(new Locale.Builder().setLanguage("en").setRegion("US").build()).format(balance);
+            String balanceFormatted = currency.format(balance);
             System.out.println(balanceFormatted);
         }
     }
